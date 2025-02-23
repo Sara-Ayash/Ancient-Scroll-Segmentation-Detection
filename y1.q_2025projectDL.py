2f971ca46a399fcb5cd83217cddeca36b5d77177
SaraAyash <sara05485@gmail.com>
2025-02-23 08:04:08 +0000

    start to train by fasterrcnn model

--- y1.q_2025projectDL.py
+++ y1.q_2025projectDL.py
@@ -1,16 +1,17 @@
 import json
 from typing import List
-from pydantic import BaseModel, Field
-# #!/usr/bin/env python3
-# import time
-# import os 
-# from PIL import Image
-# import torch
-# import torch.nn as nn
-# import torch.optim as optim
-# from torchvision import datasets, transforms
-# from torch.utils.data import DataLoader
-from torchmetrics.detection import IntersectionOverUnion
+from pydantic import BaseModel 
+import os 
+from PIL import Image
+import torch
+from torch.utils.data import DataLoader 
+from torchvision.models.detection import fasterrcnn_resnet50_fpn  
+import torchvision.transforms as T
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import torch.optim as optim
+
 
 
 
@@ -95,96 +96,92 @@ def if_intersection(xmin_a, xmax_a, ymin_a, ymax_a, xmin_b, xmax_b, ymin_b, ymax
   
 
 class AncientScrollDataset(datasets.VisionDataset):
-    def __init__(self, data_dir ):
+    def __init__(self, data_dir, include_transform=True):
         self.data_dir = data_dir
+        self.file_list = [f for f in os.listdir(data_dir) if f.lower().endswith('.jpg')]
+        self.bboxes_details_path = os.path.join(self.data_dir, 'bboxes_details')
         self.transform = None if not include_transform else self.set_transform()
-        self.file_list = os.listdir(data_dir)
         
     def __len__(self):
         return len(self.file_list)
     
     def __getitem__(self, idx):
-        file_name = self.file_list[idx].rsplit('.')
+        file_name = self.file_list[idx] 
         file_path = os.path.join(self.data_dir, file_name)
-        
+    
         # prepare image and target
-        image = self.prepare_image(file_path)
+        image = Image.open(file_path).convert("RGB")
+        
+        # Apply transformations (if any)
+        if self.transform:
+            image = self.transform(image)
         target = self.get_shapes(file_path)
         
         return image, target
     
-    def prepare_image(self, image_path):
-        image = Image.open(image_path).convert("RGB") 
-        image_tensor = F.to_tensor(image).unsqueeze(0)  
-        return image_tensor.to(device)
-    
+
+    def set_transform(self):
+        return transforms.Compose([
+            transforms.ToTensor(), 
+        ])
+ 
     def get_shapes(self, file_path):
-        shapes = []
+        bboxes, labels, shapes  = [], [], []
+        bboxes_json_path = os.path.join(self.bboxes_details_path, os.path.splitext(os.path.basename(file_path))[0] + ".json")
         
-        with open(file_path) as f:
+        with open(bboxes_json_path) as f:
             shapes =  json.load(f).get("shapes", [])
         
-        return [
-            Shape(label=shape.get('label'), bbox=sum(shape.get('points', []), []))
-            for shape in shapes
-        ]
-        
-        # for shape in shapes:
-        #     scroll_number.append(shape.get('label'))
-        #     bboxes.append(sum(shape.get('points', []), []))
+        # return [
+        #     Shape(label=shape.get('label'), bbox=sum(shape.get('points', []), []))
+        #     for shape in shapes
+        # ]
+        for shape in shapes:
+            labels.append(int(shape.get('label')))
+            bboxes.append(sum(shape.get('points', []), []))
 
-        # return {
-        #     "boxes": torch.tensor(boxes, dtype=torch.float32).to(device),
-        #     "scroll_number": torch.tensor(scroll_number, dtype=torch.int64).to(device),
-        # } 
+        return {
+            "boxes": torch.tensor(bboxes, dtype=torch.float32),
+            "labels": torch.tensor(labels, dtype=torch.float32),
+        } 
 
     
 def train_one_epoch(model, optimizer, data_loader, device, epoch):
     model.train()
+    train_loss = 0.0
     for images, targets in data_loader:
-        # extract targets
-        processed_targets, valid_images = [], []
-        for target in enumerate(targets):
-            boxes, labels = [], []
-            
-            for obj in target: 
-                bbox = obj["bbox"]     
-                bboxes_gt.append(bbox)
-                labels.append(obj["label"])
+        images = list(image.to(device) for image in images)
+        targets = [{
+            k: v.to(device) 
+            for k, v in t.items()} 
+            for t in targets]
 
-            if boxes:
-                processed_target = {
-                    "boxes": torch.tensor(boxes, dtype=torch.float32).to(device),
-                    "labels": torch.tensor(labels, dtype=torch.int64).to(device),
-                }
-                processed_targets.append(processed_target)
-        
-        images = images.to(device)
-        
-        # Forward pass
-        outputs = model(images).squeeze()
-         
-        loss_dict = model(images, processed_targets)
-        bboxes_pred = outputs["instances"].pred_boxes
-        IOUs = metric.update(bboxes_pred, processed_targets)
-        # losses = sum(loss for loss in loss_dict.values())
-
-        # Backpropagation
+        # Zero the gradients
         optimizer.zero_grad()
+
+        # Forward pass
+        loss_dict = model(images, targets)
+        losses = sum(loss for loss in loss_dict.values())
+
+        # Backward pass
         losses.backward()
         optimizer.step()
+        train_loss += losses.item()
 
     print(f"Epoch [{epoch}] Loss: {losses.item():.4f}")        
            
         
         
 def get_fasterrcnn_model():
-    # Load pre-trained Faster R-CNN
-    model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+    # Load the pre-trained Faster R-CNN model with a ResNet-50 backbone
+    model = fasterrcnn_resnet50_fpn(pretrained=True)
+
     # Get the number of input features for the classifier
     in_features = model.roi_heads.box_predictor.cls_score.in_features
-    # Replace the pre-trained head with a new one
+
+    # Replace the head of the model with a new one (for the number of classes in your dataset)
     model.roi_heads.box_predictor = FastRCNNPredictor(in_features, 1)
+
     return model
 
 class Model(BaseModel):
@@ -195,10 +192,11 @@ class Model(BaseModel):
      
 if __name__ == "__main__":
     train_dataset: datasets.ImageFolder = AncientScrollDataset('saraay@post.jce.ac.il/training_data')
-    validate_dataset: datasets.ImageFolder = AncientScrollDataset('student_318411840_v2/Validation/Validation/smoking')
+    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, collate_fn=lambda x: tuple(zip(*x)))
+    # validate_dataset: datasets.ImageFolder = AncientScrollDataset('student_318411840_v2/Validation/Validation/smoking')
     
-    train_loader = DataLoader(train_dataset, batch_size=10, shuffle=True, num_workers=4)
-    validation_loader = DataLoader(validate_dataset, batch_size=2, shuffle=True, num_workers=4)
+    # train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
+    # validation_loader = DataLoader(validate_dataset, batch_size=2, shuffle=True, num_workers=4)
     
     
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -215,8 +213,7 @@ if __name__ == "__main__":
     
     # Define optimizer and learning rate scheduler
     params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    # optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
+    optimizer = torch.optim.SGD(params, lr=0.005, momentum=0.9, weight_decay=0.0005)
     lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
     
     # Training loop
@@ -226,8 +223,8 @@ if __name__ == "__main__":
         lr_scheduler.step()
         
         # Save the model's state dictionary after every epoch
-        torch.save(model.state_dict(), model_path)
-        print(f"Model saved: {model_path}")
+        torch.save(model.state_dict(), fasterrcnn_model_path)
+        print(f"Model saved: {fasterrcnn_model_path}")
  
     
    
