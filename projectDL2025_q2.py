10d1c42c9be4ab3b012138043ef39789eb20f512
SaraAyash <sara05485@gmail.com>
2025-03-18 21:49:40 +0000

    clean

--- projectDL2025_q2.py
+++ projectDL2025_q2.py
@@ -29,8 +29,8 @@ def process_detailed_bounding_boxes(image_paths: list[str], output_csv: str) -> 
     for image_path in image_paths:
 
         detection_model = get_best_model()
-        # detection_model = get_best_model("yolov8")
-        detection_model.csv_results_path = "test.csv"
+        detection_model = get_best_model("yolov8")
+        # detection_model.csv_results_path = "test_yolo.csv"
 
         img_train_result = detection_model.evaluation(image_path)
         csv_file = f'faster_rcnn_test_{os.path.basename(image_path)}.csv'
@@ -52,6 +52,6 @@ if __name__ == "__main__":
     validation_dir = "saraay@post.jce.ac.il/test"
     image_paths = [os.path.join(validation_dir, f) for f in os.listdir(validation_dir) if f.lower().endswith('.jpg')]
     
-    process_detailed_bounding_boxes(image_paths, '')
+    process_detailed_bounding_boxes(image_paths, 'yolo_res.csv')
  
     
